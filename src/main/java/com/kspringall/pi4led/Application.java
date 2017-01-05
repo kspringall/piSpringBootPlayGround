318bae7e3d1ededd106959779e1ed722bc92d678
kspringall <kimballg@springall.name>
2017-01-05 07:29:55 -0700

    Adding files

---
+++ src/main/java/com/kspringall/pi4led/Application.java
@@ -0,0 +1,15 @@
+package com.kspringall.pi4led;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
+
+    }
+
+}
